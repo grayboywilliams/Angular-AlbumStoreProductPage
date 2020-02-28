dbd94bbdd401103d4ba90d53ba1350025d566f81
Grayson Williams <43975768+grayboywilliams@users.noreply.github.com>
2020-02-28 17:58:13 -0500

    Mod 5: fixed incorrect file update

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Album } from '../album';
-import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-page',
@@ -9,12 +7,9 @@ import { ProductService } from '../product.service';
 })
 export class ProductPageComponent implements OnInit {
 
-  albumInfo: Album;
-
-  constructor(private _productService: ProductService) { }
+  constructor() { }
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response)
   }
 
 }
